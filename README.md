85c14b8ca4e389d21e3f351ad9da35a809cf46e4
Adson Gregorio <adson.gregorio@gmail.com>
2020-05-12 20:03:31 -0300

    Implementando autenticação com JWT

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CentralDeErros
+
+Link Collection Postman: https://app.getpostman.com/join-team?invite_code=a6d287aac7ba7edcfc395f95c006755b
--- central-erros/src/main/java/br/com/codenation/controllers/ExampleController.java
+++ central-erros/src/main/java/br/com/codenation/controllers/ExampleController.java
@@ -5,12 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * ExampleController
- *
- * @author danielpadua
- *
- */
+
 @RestController
 @RequestMapping("/api/example")
 public class ExampleController {
