090b1267b601c5d4fa41d47b863e0ffff281f571
Gustavo Funchal <delagusttaf@gmail.com>
2020-06-19 23:09:05 -0300

    Swagger instalado

--- central-erros/src/main/java/br/com/codenation/controllers/UserController.java
+++ central-erros/src/main/java/br/com/codenation/controllers/UserController.java
@@ -2,6 +2,8 @@ package br.com.codenation.controllers;
 
 import br.com.codenation.model.User;
 import br.com.codenation.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/user")
+@Api(value = "Responsável pelo controle de usuários da aplicação.")
 public class UserController {
 
     @Autowired
@@ -17,6 +20,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Registra um novo usuário")
     public User create(@RequestBody User user){
         return userService.save(user);
     }
